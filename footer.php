dbe691ab4ec8a32df87e2e37a726c58e178ab509
Keith Petrillo <keith@areli.com>
2023-09-08 08:04:53 -0400

    Moved privacy policy and disclaimer to primary footer

--- footer.php
+++ footer.php
@@ -16,5 +16,27 @@ endif;
 
 <?php wp_footer(); ?>
 
+<div id="privacy-policy" class="modal" style="display: none;">
+    <div class="modal-content">
+        <i class="fa-regular fa-xmark fa-xl close"></i>
+        <div class="modal-body">
+            <div class="entry-content p-8">
+				<?php get_template_part( 'template-parts/privacy-policy' ); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="disclaimer" class="modal" style="display: none;">
+    <div class="modal-content">
+        <i class="fa-solid fa-xmark close"></i>
+        <div class="modal-body">
+            <div class="entry-content p-8">
+				<?php get_template_part( 'template-parts/disclaimer' ); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
